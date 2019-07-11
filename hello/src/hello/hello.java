587842373378a697ffadfd3cb18b1a68f7c0bdf1
Manjinder singh <manjinders2212@gmai.com>
2019-07-11 14:03:47 -0400

    first time commit using eclipse

---
+++ hello/src/hello/hello.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+	    int x= 30;
+	    switch(x) {
+	    case 10:System.out.println("10");break;
+	    case 20:System.out.println("20");break;
+	    case 30:System.out.println("30");break;
+	    default:System.out.println("NOT IN 10, 20 and 30");
+	    
+	    }
+
+	}
+
+}
